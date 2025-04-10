6c9e0f0f7da964c658e17c8fb5a36bcc6a68994f
icaroleeb <icaroleeb@gmail.com>
2025-04-09 21:00:47 -0400

    OG logo Title stuff

--- source/states/TitleState.hx
+++ source/states/TitleState.hx
@@ -576,6 +576,18 @@ class TitleState extends MusicBeatState
 	var increaseVolume:Bool = false;
 	function skipIntro():Void
 	{
+		logoBl.y += 1500;
+		logoBl.angle = -4;
+		FlxTween.tween(logoBl,{y: -100}, 1.4, {ease: FlxEase.expoInOut});
+
+		new FlxTimer().start(0.01, function(tmr:FlxTimer)
+		{
+			if(logoBl.angle == -4) 
+				FlxTween.angle(logoBl, logoBl.angle, 4, 4, {ease: FlxEase.quartInOut});
+			if (logoBl.angle == 4) 
+				FlxTween.angle(logoBl, logoBl.angle, -4, 4, {ease: FlxEase.quartInOut});
+		}, 0);
+
 		if (!skippedIntro)
 		{
 			#if TITLE_SCREEN_EASTER_EGG
