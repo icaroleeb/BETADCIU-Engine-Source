488ada2544ab702d38b4deb89107de12c4b5a5e0
icaroleeb <icaroleeb@gmail.com>
2025-10-24 16:33:03 -0400

    Hold Covers RGB & Pixeled beta

--- source/objects/HoldCover.hx
+++ source/objects/HoldCover.hx
@@ -6,6 +6,8 @@ import objects.Note;
 import flixel.group.FlxSpriteGroup.FlxTypedSpriteGroup;
 import flixel.util.FlxTimer;
 import states.PlayState;
+import shaders.RGBPalette;
+import flixel.system.FlxAssets.FlxShader;
 
 using StringTools;
 
@@ -17,7 +19,6 @@ class CoverSprite extends FlxSprite
 	public var boom:Bool = false;
 	public var isPlaying:Bool = false;
 	public var activatedSprite:Bool = true;
-	public var useRGBShader:Bool = false;
 
 	public var spriteId:String = "";
 	public var texture(default, set):String = null;
@@ -73,11 +74,13 @@ class HoldCover extends FlxTypedSpriteGroup<CoverSprite>
 {
 	public var enabled:Bool = true;
 	public var isPlayer:Bool = false;
+	public var rgbShader:PixelHoldShaderRef;
 
 	public function new(enabled:Bool, isPlayer:Bool)
 	{
 		this.enabled = enabled;
 		this.isPlayer = isPlayer;
+		
 		super(0, 0, 4);
 		for (i in 0...maxSize)
 			addHolds(i);
@@ -96,6 +99,9 @@ class HoldCover extends FlxTypedSpriteGroup<CoverSprite>
 		hold.activatedSprite = enabled;
 		hold.spriteId = '$hColor-$i';
 		this.add(hold);
+
+		rgbShader = new PixelHoldShaderRef();
+		hold.shader = rgbShader.shader;
 	}
 
 	public function spawnOnNoteHit(note:Note, isReady:Bool):Void
@@ -105,6 +111,69 @@ class HoldCover extends FlxTypedSpriteGroup<CoverSprite>
 		var isSus:Bool = note.isSustainNote;
 		var isHoldEnd:Bool = false;
 		if (note.animation.curAnim != null) isHoldEnd = note.animation.curAnim.name.endsWith('end');
+
+		// rgb shader hold cover stuff
+
+		/*
+		var tempShader:RGBPalette = null;
+		if (config.allowRGB)
+		{
+			Note.initializeGlobalRGBShader(noteData % Note.colArray.length);
+			if (inEditor || (note == null || note.noteSplashData.useRGBShader) && (PlayState.SONG == null || !PlayState.SONG.disableNoteRGB))
+			{
+				tempShader = new RGBPalette();
+				// If Note RGB is enabled:
+				if ((note == null || !note.noteSplashData.useGlobalShader) || inEditor)
+				{
+					var colors = config.rgb;
+					if (colors != null)
+					{
+						for (i in 0...colors.length)
+						{
+							if (i > 2) break;
+
+							var arr:Array<FlxColor> = ClientPrefs.data.arrowRGB[noteData % Note.colArray.length];
+							if (note != null && note.isPixelNote) arr = ClientPrefs.data.arrowRGBPixel[noteData % Note.colArray.length];
+
+							var rgb = colors[i];
+							if (rgb == null)
+							{
+								if (i == 0) tempShader.r = arr[0];
+								else if (i == 1) tempShader.g = arr[1];
+								else if (i == 2) tempShader.b = arr[2];
+								continue;
+							}
+
+							var r:Null<Int> = rgb.r; 
+							var g:Null<Int> = rgb.g;
+							var b:Null<Int> = rgb.b;
+
+							if (r == null || Math.isNaN(r) || r < 0) r = arr[0];
+							if (g == null || Math.isNaN(g) || g < 0) g = arr[1];
+							if (b == null || Math.isNaN(b) || b < 0) b = arr[2];
+
+							var color:FlxColor = FlxColor.fromRGB(r, g, b);
+							if (i == 0) tempShader.r = color;
+							else if (i == 1) tempShader.g = color;
+							else if (i == 2) tempShader.b = color;
+						}
+					}
+					else tempShader.copyValues(Note.globalRgbShaders[noteData % Note.colArray.length]);
+
+					if (note != null)
+					{
+						if (note.noteSplashData.r != -1) tempShader.r = note.noteSplashData.r;
+						if (note.noteSplashData.g != -1) tempShader.g = note.noteSplashData.g;
+						if (note.noteSplashData.b != -1) tempShader.b = note.noteSplashData.b;
+					}
+				}
+				else tempShader.copyValues(Note.globalRgbShaders[noteData % Note.colArray.length]);
+			}
+		}
+		rgbShader.copyValues(tempShader);
+		if (!config.allowPixel) rgbShader.pixelAmount = 1;
+		else if (note != null && note.isPixelNote) rgbShader.pixelAmount = 6;
+		*/
 
 		if (enabled && isReady)
 		{
@@ -227,5 +296,105 @@ class HoldCover extends FlxTypedSpriteGroup<CoverSprite>
 		}
 		}
 		return 0;
+	}
+}
+
+class PixelHoldShaderRef 
+{
+	public var shader:PixelHoldShader = new PixelHoldShader();
+	public var enabled(default, set):Bool = true;
+	public var pixelAmount(default, set):Float = 1;
+
+	public function copyValues(tempShader:RGBPalette)
+	{
+		if (tempShader != null)
+		{
+			for (i in 0...3)
+			{
+				shader.r.value[i] = tempShader.shader.r.value[i];
+				shader.g.value[i] = tempShader.shader.g.value[i];
+				shader.b.value[i] = tempShader.shader.b.value[i];
+			}
+			shader.mult.value[0] = tempShader.shader.mult.value[0];
+		}
+		else enabled = false;
+	}
+
+	public function set_enabled(value:Bool)
+	{
+		enabled = value;
+		shader.mult.value = [value ? 1 : 0];
+		return value;
+	}
+
+	public function set_pixelAmount(value:Float)
+	{
+		pixelAmount = value;
+		shader.uBlocksize.value = [value, value];
+		return value;
+	}
+
+	public function reset()
+	{
+		shader.r.value = [0, 0, 0];
+		shader.g.value = [0, 0, 0];
+		shader.b.value = [0, 0, 0];
+	}
+
+	public function new()
+	{
+		reset();
+		enabled = true;
+
+		if (!PlayState.isPixelStage) pixelAmount = 1;
+		else pixelAmount = PlayState.daPixelZoom;
+		//trace('Created shader ' + Conductor.songPosition);
+	}
+}
+
+class PixelHoldShader extends FlxShader
+{
+	@:glFragmentHeader('
+		#pragma header
+
+		uniform vec3 r;
+		uniform vec3 g;
+		uniform vec3 b;
+		uniform float mult;
+		uniform vec2 uBlocksize;
+
+		vec4 flixel_texture2DCustom(sampler2D bitmap, vec2 coord) {
+			vec2 blocks = openfl_TextureSize / uBlocksize;
+			vec4 color = flixel_texture2D(bitmap, floor(coord * blocks) / blocks);
+			if (!hasTransform) {
+				return color;
+			}
+
+			if (color.a == 0.0 || mult == 0.0) {
+				return color * openfl_Alphav;
+			}
+
+			vec4 newColor = color;
+			newColor.rgb = min(color.r * r + color.g * g + color.b * b, vec3(1.0));
+			newColor.a = color.a;
+
+			color = mix(color, newColor, mult);
+
+			if (color.a > 0.0) {
+				return vec4(color.rgb, color.a);
+			}
+			return vec4(0.0, 0.0, 0.0, 0.0);
+		}')
+
+	@:glFragmentSource('
+		#pragma header
+
+		void main() {
+			gl_FragColor = flixel_texture2DCustom(bitmap, openfl_TextureCoordv);
+		}')
+
+	public function new()
+	{
+		super();
 	}
 }
